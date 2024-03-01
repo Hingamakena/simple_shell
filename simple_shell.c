219f3e58a4f566d309919bac25da149937abdf28
hingamakena <hingamakena@gmail.com>
2024-02-29 22:00:02 -0800

    bettied the simple shell file

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: (0) success
+ */
+
 int main(__attribute__ ((unused))int argc, __attribute__((unused))char **argv)
 {
 	pid_t my_pid;
@@ -10,12 +18,12 @@ int main(__attribute__ ((unused))int argc, __attribute__((unused))char **argv)
 
 	my_pid = getpid();
 
-	write(1,"$: ",3);
+	write(1, "$: ", 3);
 
-	if((nread = getline(&line, &len, stdin)) == -1)
+	if ((getline(&line, &len, stdin)) == -1)
 	{
 		printf("error reading input command");
-	}	
+	}
 	fwrite(line, nread, 1, stdout);
 
 	ts = strtok(line, " ");
@@ -24,7 +32,7 @@ int main(__attribute__ ((unused))int argc, __attribute__((unused))char **argv)
 	printf("%s", ts);
 	printf("\n");
 
-	printf("my pid is %u\n",my_pid);
+	printf("my pid is %u\n", my_pid);
 
 	free(line);
 	exit(EXIT_SUCCESS);
